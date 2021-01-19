2f41bf7e4da9101e16f2593d91e4b60bdbe2e23f
JeanFenner <jeancarlo.fenner@gmail.com>
2021-01-18 22:39:38 -0300

    Funcionando Ler Sequencias de Tokens

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 Universidade Federal da Fronteira Sul - UFFS
 Linguagem Formal e Autômatos
 Professor Braulio Mello
-Aluno Jean Carlo Fenner
+Alunos Elvis de Souza Machado e Jean Carlo Fenner
 
 
 Construção de uma aplicação para construção, determinização e minimização (eliminação de mortos e inalcançáveis) de autômatos finitos.
@@ -21,150 +21,80 @@ using namespace std;
 
 class Producoes{
     public:
-        string nt;
-        string p;
-        int estado_final;
+        string estado;                      // Estado atual
+        string p;                           // Produção/terminal
+        string nt;                          // Não-Terminal
+        int estado_final;                   // Estado Final?
 
-        void adicionar(string new_nt, string new_p, int ef){
-            nt = new_nt;
+        void adicionar(string new_e, string new_p, string new_nt, int new_ef = 0){
+            estado = new_e;
             p = new_p;
-            estado_final = ef;
+            nt = new_nt;
+            estado_final = new_ef;
         };
 
-        string get_nterminal(){
-            return nt;
-        };
+        string get_estado(){
+            return estado;
+        }
         string get_prod(){
             return p;
+        };
+        string get_nterminal(){
+            return nt;
         };
         bool get_ef(){
             return estado_final;
         }
 };
 
-/*
-void txtleitor(string line, vector<char> *terminais, vector<char> *nterminais, vector<Producoes> *prod){
-    int i=0;
-    int ascii[127] = {0};
-    char simbolo;
-
-for(i=0; i<127; i++){
-    cout << ascii[i] << " ";
-    if(!(i%16))
-        cout << endl;
-}
-
-    nterminais->push_back('S');
-
-    while(line[i]){
-        simbolo = line[i];
-        if(line[0] == '<'){
-            cout << "gramatica\n";
-        } else{
-            cout << "tokens\n";
-            ascii[simbolo] = 1;
-
-            if(i==0)
-                Producoes p(nterminais->begin(), simbolo);
-            
-        }
-
-
-        i++;
-    }
-
-    for(i=0; i<127; i++){
-        if(ascii[i])
-            terminais->push_back(ascii[i]);
-    }
-    
-    return;
-};
-*/
-
 int main(){
-    int i, ntchar[2];
-    int ascii[127] = {0};
-    string simbolot;
-    string simbolont;
-    string simbolopar;
-    string txtline;
-    vector<char> terminais;
-    vector<char> nterminais;
-    vector<Producoes> prod;
-    Producoes par;
+    int i;
+    int ntchar[2];                          // Character do Não Terminal
+    int ascii[127] = {0};                   // Lista de uso ASCII
+    string simboloe;                        // Simbolo Estado
+    string simbolot;                        // Simbolo Terminal
+    string simbolont;                       // Simbolo Não-Terminal
+    string simbolopar;                      // Simbolo Par da Produção
+    string txtline;                         // Linha do .txt
+    vector<char> terminais;                 // Vetor de Terminais
+    vector<string> nterminais;              // Vetor de Não-Terminais
+    vector<Producoes> prod;                 // Relação de T/NT
+    Producoes par;                          // Item de Vetor <prod>
 
     ifstream txtfiles("text.txt");
     
     if(txtfiles.is_open()){
 
-        nterminais.push_back('S');
+        nterminais.push_back("S");
         ntchar[0] = 'A';
 
         while(getline(txtfiles, txtline)){
             cout << txtline << "\n";
 
-//            txtleitor(txtline, &terminais, &nterminais, &prod);
-
             if(txtline[0] == '<'){
 // GRAMATICAS
-                for(i=1; i<txtline.length(); i++){
-                    cout << txtline[i] << " ";
-                    if(i=1){
-                        if(txtline[i] == 'S')
-                            simbolont = *simbolont.begin();
-                        else
-                            simbolont = ntchar[0]++;
-                        i += 2;
-                    }else if(txtline[i+1]==':' && txtline[i+2]=='='){
-                        i += 3;
-                    } else{
-                        switch (txtline[i]){
-                            case ' ':
-                                break;
-                            case '<':
-                                break;
-                            case '>':
-                                break;
-                            case '|':
-                                break;
-                            case '&':
-                                simbolopar = txtline[i];
-                                par.adicionar(simbolont, simbolopar, 1);
-
-                                break;
-                            default:
-                                simbolopar = txtline[i];
-                                ascii[txtline[i]] = 1;
-                                par.adicionar(simbolont, simbolopar, 0);
-
-                                break;
-                        }
-                    }
-                }
-                cout << endl;
             } else{
 // TOKENS
-                simbolont = *nterminais.begin();
+                simboloe = *nterminais.begin();
 
                 for(i=0; i<txtline.length(); i++){
-        
-
                     simbolot = txtline[i];
+                    simbolont = *ntchar;
                     ascii[txtline[i]] = 1;
-
-                    if(i>0)
-                        simbolont = ntchar[0]++;
                         
-                    par.adicionar(simbolont, simbolot, 0);
+                    par.adicionar(simboloe, simbolot, simbolont);
                     prod.push_back(par);
+
+                    cout << simboloe << " " << simbolot << " " << simbolont << endl;
                     
-                    cout << simbolont << " " << simbolot << endl;
+
+                    ntchar[0]++;
+                    simboloe = simbolont;
+                    nterminais.push_back(simbolont);
                 }
-                    simbolont = ntchar[0]++;
-                    simbolot = '\n';
-                    par.adicionar(simbolont, simbolot, 1);
-                    prod.push_back(par);
+                simbolot = "eps";
+                par.adicionar(simboloe, simbolot, " ", 1);
+                prod.push_back(par);
             }
 
             if(ntchar[0]>90){
@@ -183,21 +113,27 @@ int main(){
             terminais.push_back(i);
     }
 
-    for(i=0; i<nterminais.size(); i++)
-        cout << nterminais[i] << " ";
-    cout << endl;
     for(i=0; i<terminais.size(); i++)
         cout << terminais[i] << " ";
+    cout << endl;
+    for(i=0; i<nterminais.size(); i++)
+        cout << nterminais[i] << " ";
     cout << endl;
 
     cout << "Producoes\n";
     vector<Producoes>::iterator it;
     for(it=prod.begin(); it<prod.end(); it++){
         if((*it).get_ef())
-            cout << "*" << (*it).get_nterminal() << " ";
+            cout << "*" << (*it).get_estado() << " ";
         else
-            cout << (*it).get_nterminal() << " ";
+            cout << (*it).get_estado() << " ";
     }
+    cout << endl;
+
+    for(it=prod.begin(); it<prod.end(); it++){
+        cout << (*it).get_prod() << " ";
+    }
+    cout << endl;
 
     return 0;
 }
