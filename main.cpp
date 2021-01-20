e646aae24a289deb5da7948659ed73ae3e74fb2d
JeanFenner <jeancarlo.fenner@gmail.com>
2021-01-20 13:50:22 -0300

    Leitura e Construção Prontas

--- main.cpp
+++ main.cpp
@@ -12,6 +12,8 @@ Construção de uma aplicação para construção, determinização e minimizaç
 #include <fstream>
 #include <string>
 #include <vector>
+#define ARQUIVO "text.txt"              // Arquivo que será lido
+#define EPS "&"                         // Epsilon
 
 using namespace std;
 
@@ -40,13 +42,14 @@ class Producoes{
         string get_nterminal(){
             return nt;
         };
-        bool get_ef(){
+        int get_ef(){
             return estado_final;
         }
 };
 
 class Table{
     vector <vector<string>> table;
+    vector <int> estado_final;
 
     public:
         Table(int lin, int col){
@@ -58,29 +61,34 @@ class Table{
                     nt.push_back(" ");
                 table.push_back(nt);
             }
+            for(i=0; i<lin; i++)
+                estado_final.push_back(0);
 
         }
 
-        void adicionar(int lin, int col, string novo){
+        void adicionar(int lin, int col, int ef, string novo = " "){
             table[col][lin].append(novo+" ");
+            if(ef)
+                estado_final[lin] = 1;
 
             return;
         }
 
         void print_table(int lin, int col){
-            int i, j;
-            for(i=0; i<lin; i++){
-                for(j=0; j<col; j++){
-                    cout << table[j][i] << "|\t";
+            for(int i=0; i<lin; i++){
+                for(int j=0; j<col; j++){
+                    cout << table[j][i] << " ";
                 }
                 cout << endl;
             }
-
-            return;
         }
 
         string get_cel(int lin, int col){
-            return table[lin][col];
+            return table[col][lin];
+        }
+
+        bool get_ef(int lin){
+            return estado_final[lin];
         }
 };
 
@@ -116,7 +124,7 @@ class Gra_Tab{
 int main(){
     int i, j;
     int ntchar[2];                          // Character do Não Terminal
-    int ascii[127] = {0};                   // Lista de uso ASCII
+    int ascii[129] = {0};                   // Lista de uso ASCII
     string simboloe;                        // Simbolo Estado
     string simbolot;                        // Simbolo Terminal
     string simbolont;                       // Simbolo Não-Terminal
@@ -132,8 +140,8 @@ int main(){
     vector <Producoes>::iterator it_p;      // Iterador do Vetor <prod>
     vector <Gra_Tab>::iterator it_gt;       // Iterador do Vetor <gra_tab>
 
-    ifstream txtfiles("text.txt");
-    
+    ifstream txtfiles(ARQUIVO);
+
     if(txtfiles.is_open()){
 
         nterminais.push_back("S");
@@ -151,18 +159,10 @@ int main(){
                 string auxchar;
                 j = 1;
 
-                cout << endl;
-                for(i=0; i<gra_tab.size(); i++){
-                    cout << gra_tab[i].get_g() << "_" << gra_tab[i].get_t() << " ";
-                }
-                cout << endl;
-
                 while(txtline[j] != '>'){
                     estado.push_back(txtline[j]);
                     j++;
                 }
-                cout << "G\nestado: " << estado << "\t";
-
                 for(it_gt=gra_tab.begin(); it_gt<gra_tab.end(); it_gt++){
                     if((*it_gt).compara(estado)){
                         estado = (*it_gt).get_t();
@@ -178,74 +178,74 @@ int main(){
                 }
                 simboloe = estado;
                 estado = "NUL";
-                cout << "simboloe: " << simboloe << endl;
 
                 while (txtline[j] != '='){
                     j++;
                 }
 
                 for(i=j+1; i<txtline.length(); i++){
-                    txtchar = txtline[i];
-                    cout << txtchar << "[";
-                    if(txtchar>32 && txtchar<127){
-                        if(txtchar == '|'){
-                            par.adicionar(simboloe, simbolot, simbolont);
-                            prod.push_back(par);
-                            cout << "+";
-                            if(ef){
-                                par.adicionar(simbolont, "eps", "NUL", ef);
-                                prod.push_back(par);
-                                nterminais.push_back(simbolont);
-                                ntchar[0]++;
-                                ef = 0;
-                                cout << "*" << simbolont;
-                            }
-                        }else if(txtchar=='<'){
-                            i++;
-                            while(txtline[i] != '>'){
-                                auxchar = txtline[i];
+                    if(txtline[i]>32 && txtline[i]<129){
+                        while(txtline[i] !='|' && i < txtline.length()){
+                            if(txtline[i]=='<'){
                                 i++;
-                            }
+                                while(txtline[i] != '>'){
+                                    auxchar = txtline[i];
+                                    i++;
+                                }
 
-                            for(it_gt=gra_tab.begin(); it_gt<gra_tab.end(); it_gt++){
-                                if((*it_gt).compara(auxchar)){
-                                    simbolont = (*it_gt).get_t();
-                                    it_gt = gra_tab.end();
-                                } else if((it_gt+1) == gra_tab.end()){
-                                    simbolont = (*ntchar);
-                                    dupla.adicionar(auxchar, simbolont);
-                                    gra_tab.push_back(dupla);
-                                    nterminais.push_back(simbolont);
-                                    ntchar[0]++;
+                                for(it_gt=gra_tab.begin(); it_gt<gra_tab.end(); it_gt++){
+                                    if((*it_gt).compara(auxchar)){
+                                        simbolont = (*it_gt).get_t();
+                                        it_gt = gra_tab.end();
+                                    } else if((it_gt+1) == gra_tab.end()){
+                                        simbolont = (*ntchar);
+                                        dupla.adicionar(auxchar, simbolont);
+                                        gra_tab.push_back(dupla);
+                                        nterminais.push_back(simbolont);
+                                        ntchar[0]++;
+                                    }
+                                }
+                            } else if(txtline[i]>32 && txtline[i]<127){
+                                simbolot = txtline[i];
+                                if(simbolot != EPS)
+                                    ascii[txtline[i]] = 1;
+                                if(txtline[i+1] != '<'){
+                                    ef = 1;
                                 }
                             }
-                            cout << auxchar << " " << simbolont << " ";
-                        }else {
-                            simbolot = txtchar;
-                            ascii[txtchar] = 1;
-                            cout << txtchar << " ";
-                            if(txtline[i+1]!='<'){
-                                simbolont = *ntchar;
-                                ntchar[0]++;
-                                ef = 1;
-                            }
+                            i++;
                         }
+                        if(ef){
+                            if(simbolot == EPS){
+                                par.adicionar(simboloe, simbolot, "NUL", 1);
+                                prod.push_back(par);
+                            }else{
+                                simbolont = (*ntchar);
+                                nterminais.push_back(simbolont);
+                                ntchar[0]++;
+
+                                par.adicionar(simboloe, simbolot, simbolont);
+                                prod.push_back(par);
+                                par.adicionar(simbolont, EPS, "NUL", 1);
+                                prod.push_back(par);
+                            }
+                            ef = 0;
+                        }else{
+                            par.adicionar(simboloe, simbolot, simbolont);
+                            prod.push_back(par);
+                        }
+                        
+                        // Adicona ' aos novos NTs case passe de Z
+                        if(ntchar[0]>90){
+                            ntchar[0] = 65;
+                            ntchar[1] = 39;
+                        }
+
+                        i++;
                     }
-                    cout << "] ";
                 }
-                par.adicionar(simboloe, simbolot, simbolont);
-                prod.push_back(par);
-                cout << "+";
-                if(ef){
-                    par.adicionar(simbolont, "eps", "NUL", ef);
-                    prod.push_back(par);
-                    nterminais.push_back(simbolont);
-                    ntchar[0]++;
-                    ef = 0;
-                    cout << "*" << simbolont;
-                }
-                cout << endl;
-            } else{                             // TOKENS
+            }
+            else{                             // TOKENS
 
                 simboloe = *nterminais.begin();
 
@@ -253,12 +253,8 @@ int main(){
                     simbolot = txtline[i];
                     simbolont = *ntchar;
                     ascii[txtline[i]] = 1;
-                        
                     par.adicionar(simboloe, simbolot, simbolont);
                     prod.push_back(par);
-
-                    cout << simboloe << " " << simbolot << " " << simbolont << endl;                    
-
                     ntchar[0]++;
                     simboloe = simbolont;
                     nterminais.push_back(simbolont);
@@ -267,26 +263,27 @@ int main(){
                     if(ntchar[0] == 'S' && ntchar[1] != 39)
                         ntchar[0]++;
                 }
-                simbolot = "eps";
+                simbolot = EPS;
                 par.adicionar(simboloe, simbolot, "NUL", 1);
                 prod.push_back(par);
+                
+                // Adicona ' aos novos NTs case passe de Z
+                if(ntchar[0]>90){
 
-                cout << "*" << simboloe << " " << simbolot << " " << simbolont << endl;
+                    ntchar[0] = 65;
+                    ntchar[1] = 39;
+                }
             }
-
-            // Adicona ' aos novos NTs case passe de Z
-            if(ntchar[0]>90){
-                ntchar[0] = 65;
-                ntchar[1] = 39;
-            }
-
             cout << endl;
         }
         txtfiles.close();
-    } else
+    } else{
         cout << "Impossivel abrir arquivo\n";
+        
+        return 0;
+    }
 
-// Populando vetor de terminais com os caracteres da ascii utilizados
+// Preenchendo vetor de terminais com os caracteres da ascii utilizados
     for(i=0; i<127; i++){
         if(ascii[i]!=0){
             char ctemp = i;
@@ -307,45 +304,65 @@ int main(){
     cout << "Producoes\n";
     for(it_p=prod.begin(); it_p<prod.end(); it_p++){
         if((*it_p).get_ef())
-            cout << "*" << (*it_p).get_estado() << " ";
+            cout << "*" << (*it_p).get_estado() << "\t";
         else
-            cout << (*it_p).get_estado() << " ";
+            cout << (*it_p).get_estado() << "\t";
     }
     cout << endl;
     for(it_p=prod.begin(); it_p<prod.end(); it_p++){
-        cout << (*it_p).get_prod() << " ";
+        cout << (*it_p).get_prod() << "\t";
+    }
+    cout << endl;
+    for(it_p=prod.begin(); it_p<prod.end(); it_p++){
+        cout << (*it_p).get_nterminal() << "\t";
+    }
+    cout << endl;
+    for(it_p=prod.begin(); it_p<prod.end(); it_p++){
+        cout << (*it_p).get_ef() << "\t";
     }
     cout << endl;
 
     const int t_size = terminais.size();
     const int nt_size = nterminais.size();
 
+    cout << "Criando AFND\n";
     Table table(nt_size, t_size);
 
-// Popular Tabela AFND
-    for(it_p=prod.begin(); it_p < prod.end(); it_p++){
-        for(i=0; i < nt_size; i++){
-            if((*it_p).get_estado() == nterminais[i]){
+    cout << "AFND criada\nPreenchendo tabela\n";
+
+// Preencher Tabela AFND
+    for(it_p=prod.begin(); it_p<prod.end(); it_p++){
+        int ef_aux = (*it_p).get_ef();
+        string e_aux = (*it_p).get_estado();
+        string p_aux = (*it_p).get_prod();
+        string nt_aux = (*it_p).get_nterminal();
+
+        for(i=0; i<nt_size; i++){
+            if(e_aux == nterminais[i]){
                 for(j=0; j<t_size; j++){
-                    if((*it_p).get_prod() == terminais[j]){
-                        table.adicionar(i, j, (*it_p).get_nterminal());
+                    if(p_aux == terminais[j]){
+                        cout << e_aux << " " << p_aux << " " << ef_aux << " " << nt_aux << endl;
+                        table.adicionar(i, j, ef_aux, nt_aux);
+                    }else if(p_aux == "&"){
+                        table.adicionar(i, j, ef_aux);
                     }
                 }
             }
         }
     }
+    cout << "AFND preenchida" << endl;
 
 // Tabela AFND
     cout << endl;
     cout << "Tabela AFND\n";
-    cout << "\t| ";
+    cout << "NT / T\t| ";
     for(i=0; i<t_size; i++)
         cout << terminais[i] << "\t| ";
     cout << endl;
     for(i=0; i<nt_size; i++){
-        cout << nterminais[i] << "\t| ";
+        cout << ((table.get_ef(i)) ? "*" : " ") << nterminais[i] << "\t| ";
         for(j=0; j<t_size; j++)
-            cout << table.get_cel(j, i) << "\t| ";
+            cout << table.get_cel(i, j) << "\t| ";
         cout << endl;
     }
 
